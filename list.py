347c36ec6a2d3c692b4f204d2512e412251646ff
esraltintas <es.altintas@gmail.com>
2015-03-12 22:40:11 +0200

    Odevler eklendi

--- list.py
+++ list.py
@@ -6,6 +6,7 @@ x = input("Enter a number: ")
 list = [random.randint(0,100) for r in xrange(x)] # v1
 print list
 
+list.sort()
 y = input("Kacinci elemani istersiniz?")
 
 print y, ".eleman =  ", list[y-1]
